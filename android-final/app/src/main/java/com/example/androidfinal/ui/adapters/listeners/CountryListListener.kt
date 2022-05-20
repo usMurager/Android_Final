aac150f97121d8cf29b9a7076de825b9ee956579
usMurager <86411871+usMurager@users.noreply.github.com>
2022-05-20 11:39:25 +0600

    delete

--- android-final/app/src/main/java/com/example/androidfinal/ui/adapters/listeners/CountryListListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.androidfinal.ui.adapters.listeners
-
-import com.example.androidfinal.data.remote.models.CountryListApi
-
-interface CountryListListener {
-    fun getCountry(country: CountryListApi)
-}
